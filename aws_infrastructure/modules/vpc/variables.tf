3195a935dc36bfffc1058ae8eb451cb35381a8ef
A.A <37187773+bowale01@users.noreply.github.com>
2024-06-15 13:54:12 +0200

    Update variables.tf

--- aws_infrastructure/modules/vpc/variables.tf
+++ aws_infrastructure/modules/vpc/variables.tf
@@ -1,1 +1,11 @@
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+}
 
+variable "public_subnets" {
+  description = "A list of public subnet CIDR blocks"
+}
+
+variable "private_subnets" {
+  description = "A list of private subnet CIDR blocks"
+}
