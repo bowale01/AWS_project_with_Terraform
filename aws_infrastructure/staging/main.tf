7da04a663ce74c26a2a7fac8786d11a0519ac9ec
A.A <37187773+bowale01@users.noreply.github.com>
2024-06-15 15:23:10 +0200

    Update main.tf

--- aws_infrastructure/staging/main.tf
+++ aws_infrastructure/staging/main.tf
@@ -1,52 +1,33 @@
 provider "aws" {
-  region = var.region
+  region = "eu-west-1"
 }
 
 module "vpc" {
-  source      = "../modules/vpc"
-  vpc_cidr    = var.vpc_cidr
-  public_subnets  = var.public_subnets
-  private_subnets = var.private_subnets
+  source = "../modules/vpc"
+  env    = "staging"
 }
 
 module "ec2" {
   source      = "../modules/ec2"
   vpc_id      = module.vpc.vpc_id
-  public_subnet_id = element(module.vpc.public_subnets, 0)
-  key_name    = var.key_name
+  subnet_ids  = module.vpc.public_subnets
+  environment = "staging"
 }
 
 module "rds" {
-  source                = "../modules/rds"
-  vpc_id                = module.vpc.vpc_id
-  private_subnet_ids    = module.vpc.private_subnets
-  db_username           = var.db_username
-  db_password           = var.db_password
+  source      = "../modules/rds"
+  vpc_id      = module.vpc.vpc_id
+  subnet_ids  = module.vpc.private_subnets
+  environment = "staging"
 }
 
 module "s3" {
   source      = "../modules/s3"
-  bucket_name = "${var.environment}-sensitive-data-bucket"
+  environment = "staging"
 }
 
 module "lambda" {
-  source                = "../modules/lambda"
-  bucket_name           = module.s3.bucket_name
-  secret_name           = var.db_secret_name
-  s3_bucket_name        = "${var.environment}-sensitive-data-bucket"
-}
-
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "my-terraform-state-bucket"
-  acl    = "private"
-  versioning {
-    enabled = true
-  }
-}
-
-resource "aws_s3_bucket_object" "state" {
-  bucket = aws_s3_bucket.terraform_state.bucket
-  key    = "${var.environment}/terraform.tfstate"
-  source = "terraform.tfstate"
-  acl    = "private"
+  source      = "../modules/lambda"
+  s3_bucket   = module.s3.bucket_id
+  environment = "staging"
 }
