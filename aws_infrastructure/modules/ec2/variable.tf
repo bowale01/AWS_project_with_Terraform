7d7dda98502cd1a4ef15b6f34a2f24fe591e9e03
A.A <37187773+bowale01@users.noreply.github.com>
2024-06-15 13:54:51 +0200

    Update variable.tf

---
+++ aws_infrastructure/modules/ec2/variable.tf
@@ -0,0 +1,11 @@
+variable "vpc_id" {
+  description = "The ID of the VPC"
+}
+
+variable "public_subnet_id" {
+  description = "The ID of the public subnet"
+}
+
+variable "key_name" {
+  description = "The key name for SSH access"
+}
